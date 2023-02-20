0d360bb29886772095c14c52c127307df776ac42
Thiago MM <thiagommergener@gmail.com>
2023-02-20 09:37:43 -0300

    adjustments

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\{
+    Home,
+    ShowTweets
+};
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', Home::class);
 
-Route::get('/', function () {
-    return view('layout/app');
-});
+Route::get('tweets', ShowTweets::class);
